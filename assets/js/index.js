e5c6f376d3cfcc86895ca0dba06b09ba0d8799bd
Gabriel Rocha <gabriel.rocha20@live.com>
2023-01-15 01:16:27 -0300

    disable scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,13 @@
 const cabecalhoMenu = document.querySelector('.cabecalho__menu')
 const menuLateral = document.querySelector('.menu-lateral')
+const body = document.querySelector("body");
 cabecalhoMenu.addEventListener('click', () => {
     menuLateral.classList.toggle('menu-lateral--ativo')
+    if (menuLateral.classList.contains("menu-lateral--ativo")) {
+        // Disable scroll
+        body.style.overflow = "hidden";
+    } else {
+        // Enable scroll
+        body.style.overflow = "auto";
+    }
 })

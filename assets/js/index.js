ab24f483b66d3887148a7c20b940ce69867f2800
Gabriel Rocha <gabriel.rocha20@live.com>
2023-01-17 13:58:54 -0300

    Responsividade do body para 1440px

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,11 @@ const menuLateral = document.querySelector('.menu-lateral')
 const body = document.querySelector("body");
 cabecalhoMenu.addEventListener('click', () => {
     menuLateral.classList.toggle('menu-lateral--ativo')
-    if (menuLateral.classList.contains("menu-lateral--ativo")) {
-        // Disable scroll
-        body.style.overflow = "hidden";
-    } else {
-        // Enable scroll
-        body.style.overflow = "auto";
-    }
+    // if (menuLateral.classList.contains("menu-lateral--ativo")) {
+    //     // Disable scroll
+    //     body.style.overflow = "hidden";
+    // } else {
+    //     // Enable scroll
+    //     body.style.overflow = "auto";
+    // }
 })
